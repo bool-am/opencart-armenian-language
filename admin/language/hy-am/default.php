d640899050f1c07edbb8c08f4b7de3c04fb392ab
hrayr <i@hrayr.am>
2024-02-16 00:40:07 +0400

    fix: translation in russian and english changed to armenian

---
+++ admin/language/hy-am/default.php
@@ -0,0 +1,167 @@
+<?php
+// Locale
+$_['code']                          = 'hy-AM';
+$_['direction']                     = 'ltr';
+$_['date_format_short']             = 'd.m.Y';
+$_['date_format_long']              = 'l, d F Y';
+$_['time_format']                   = 'H:i:s';
+$_['datetime_format']               = 'd/m/Y H:i:s';
+$_['decimal_point']                 = '.';
+$_['thousand_point']                = '';
+
+// Text
+$_['text_yes']                      = 'Այո';
+$_['text_no']                       = 'Ոչ';
+$_['text_enabled']                  = 'Միացված';
+$_['text_disabled']                 = 'Անջատված';
+$_['text_none']                     = ' --- Չընտրված --- ';
+$_['text_select']                   = ' --- Ընտրել --- ';
+$_['text_select_all']               = 'Նշել ամբողջը';
+$_['text_unselect_all']             = 'Հանել նշումները';
+$_['text_all_zones']                = 'Բոլոր տարածքները';
+$_['text_default']                  = ' <b>(Լռելայն)</b>';
+$_['text_close']                    = 'Փակել';
+$_['text_pagination']               = 'Показано с %d по %d из %d (страниц: %d)';
+$_['text_loading']                  = 'Загрузка...';
+$_['text_no_results']               = 'Տվյալներ չկան!';
+$_['text_confirm']                  = 'Данное действие необратимо. Вы уверены?';
+$_['text_home']                     = '<i class="fa fa-home fa-lg"></i>';
+
+// Button
+$_['button_add']                    = 'Ավելացնել';
+$_['button_delete']                 = 'Ջնջել';
+$_['button_save']                   = 'Պահպանել';
+$_['button_cancel']                 = 'չեղարկել';
+$_['button_cancel_recurring']       = 'Չեղարկել պռոֆիլը';
+$_['button_continue']               = 'Շարունակել';
+$_['button_clear']                  = 'Մաքրել';
+$_['button_close']                  = 'Փակել';
+$_['button_enable']                 = 'Միացնել';
+$_['button_disable']                = 'Անջատել';
+$_['button_filter']                 = 'Ֆիլտր';
+$_['button_send']                   = 'Ուղարկել';
+$_['button_edit']                   = 'Խմբագրել';
+$_['button_copy']                   = 'Կրկնօրինակել';
+$_['button_back']                   = 'Հետ';
+$_['button_remove']                 = 'Ջնջել';
+$_['button_refresh']                = 'Թարմացնել';
+$_['button_export']                 = 'Արտահանել';
+$_['button_import']                 = 'Ներմուծել';
+$_['button_download']               = 'Ներբեռնել';
+$_['button_rebuild']                = 'Նորոգել';
+$_['button_upload']                 = 'Բեռնել';
+$_['button_submit']                 = 'Պահպանել';
+$_['button_invoice_print']          = 'Ցուցադրել հաշիվը';
+$_['button_shipping_print']         = 'Տպել պատվերների ցուցակը';
+$_['button_address_add']            = 'Ավելացնել';
+$_['button_attribute_add']          = 'Ավելացնել';
+$_['button_banner_add']             = 'Ավելացնել';
+$_['button_custom_field_value_add'] = 'Ավելացնել';
+$_['button_product_add']            = 'Ավելացնել';
+$_['button_filter_add']             = 'Ավելացնել';
+$_['button_option_add']             = 'Ավելացնել';
+$_['button_option_value_add']       = 'Ավելացնել';
+$_['button_recurring_add']          = 'Ավելացնել';
+$_['button_discount_add']           = 'Ավելացնել';
+$_['button_special_add']            = 'Ավելացնել';
+$_['button_image_add']              = 'Ավելացնել';
+$_['button_geo_zone_add']           = 'Ավելացնել';
+$_['button_history_add']            = 'Ավելացնել';
+$_['button_translation']            = 'Загрузить перевод по умолчанию';
+$_['button_translation_add']        = 'Ավելացնել перевод';
+$_['button_transaction_add']        = 'Ավելացնել';
+$_['button_route_add']              = 'Ավելացնել';
+$_['button_rule_add']               = 'Ավելացնել';
+$_['button_module_add']             = 'Ավելացնել';
+$_['button_link_add']               = 'Ավելացնել';
+$_['button_approve']                = 'Одобрить';
+$_['button_deny']                   = 'Отклонить';
+$_['button_reset']                  = 'Сбросить';
+$_['button_generate']               = 'Генерировать';
+$_['button_voucher_add']            = 'Ավելացնել սերտիֆիկատ';
+$_['button_reward_add']             = 'Ավելացնել բոնուսային միավորներ';
+$_['button_reward_remove']          = 'Ջնջել բոնուսային միավորներ';
+$_['button_commission_add']         = 'Ավելացնել պահումներ';
+$_['button_commission_remove']      = 'Ջնջել պահումներ';
+$_['button_credit_add']             = 'Ավելացնել կրեդիտ';
+$_['button_credit_remove']          = 'Ջնջել կրեդիտ';
+$_['button_ip_add']                 = 'Ավելացնել IP';
+$_['button_parent']                 = 'на Уровень вверх';
+$_['button_folder']                 = 'Новая Директория';
+$_['button_search']                 = 'Поиск';
+$_['button_view']                   = 'Посмотреть';
+$_['button_install']                = 'Активировать';
+$_['button_uninstall']              = 'Деактивировать';
+$_['button_link']                   = 'Ссылка';
+$_['button_currency']               = 'Обновить значение валюты';
+$_['button_apply']                  = 'Применить';
+$_['button_category_add']           = 'Ավելացնել категорию';
+$_['button_order']                  = 'Просмотр заказа';
+$_['button_order_recurring']        = 'Показать периодические заказы';
+$_['button_buy']                    = 'Купить';
+
+// Tab
+$_['tab_affiliate']                 = 'Партнер';
+$_['tab_address']                   = 'Адрес';
+$_['tab_additional']                = 'Дополнительно';
+$_['tab_admin']                     = 'Администратор';
+$_['tab_attribute']                 = 'Атрибуты';
+$_['tab_customer']                  = 'Клиент';
+$_['tab_comment']                   = 'Комментарий';
+$_['tab_data']                      = 'Данные';
+$_['tab_description']               = 'Описание';
+$_['tab_design']                    = 'Дизайн';
+$_['tab_discount']                  = 'Скидка';
+$_['tab_documentation']             = 'Документация';
+$_['tab_general']                   = 'Основное';
+$_['tab_history']                   = 'История';
+$_['tab_ftp']                       = 'FTP';
+$_['tab_ip']                        = 'IP адрес';
+$_['tab_links']                     = 'Связи';
+$_['tab_log']                       = 'Лог';
+$_['tab_image']                     = 'Изображения';
+$_['tab_option']                    = 'Опции';
+$_['tab_server']                    = 'Сервер';
+$_['tab_seo']                       = 'SEO';
+$_['tab_store']                     = 'Магазин';
+$_['tab_special']                   = 'Акция';
+$_['tab_session']                   = 'Сессии';
+$_['tab_local']                     = 'Локализация';
+$_['tab_mail']                      = 'Почта';
+$_['tab_module']                    = 'Модули';
+$_['tab_payment']                   = 'Оплата';
+$_['tab_product']                   = 'Товары';
+$_['tab_reward']                    = 'Бонусы';
+$_['tab_shipping']                  = 'Доставка';
+$_['tab_total']                     = 'Итоги';
+$_['tab_transaction']               = 'Транзакции';
+$_['tab_voucher']                   = 'Сертификаты';
+$_['tab_sale']                      = 'Продажи';
+$_['tab_marketing']                 = 'Аналитика';
+$_['tab_online']                    = 'Кто в Online';
+$_['tab_activity']                  = 'Последняя активность';
+$_['tab_recurring']                 = 'Периодичность';
+$_['tab_action']                    = 'Действие';
+$_['tab_google']                    = 'Google';
+
+// Error
+$_['error_exception']               = 'Ошибка кода (%s): %s в %s на строке %s';
+$_['error_upload_1']                = 'Предупреждение: Размер загружаемого файла превышает значение upload_max_filesize в php.ini!';
+$_['error_upload_2']                = 'Предупреждение: Загруженный файл превышает MAX_FILE_SIZE значение, которая была указана в настройках!';
+$_['error_upload_3']                = 'Предупреждение: Загруженные файлы были загружены лишь частично!';
+$_['error_upload_4']                = 'Предупреждение: Нет файлов для загрузки!';
+$_['error_upload_6']                = 'Предупреждение: Временная папка!';
+$_['error_upload_7']                = 'Предупреждение: Ошибка записи!';
+$_['error_upload_8']                = 'Предупреждение: Запрещено загружать файл с данным расширением!';
+$_['error_upload_999']              = 'Предупреждение: Неизвестная ошибка!';
+$_['error_curl']                    = 'CURL: Ошибка кода(%s): %s';
+
+
+/* Когда нужен перевод скриптов, просто добавь код языка */
+
+// Summernote
+$_['summernote']                    = 'hy-AM';
+
+// Datepicker
+$_['datepicker']                    = 'am';
+
